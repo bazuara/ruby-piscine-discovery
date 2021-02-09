d8436856ec5728088c6405b4ddeb563caed3e52f
Bruno Azuara <bruno.azuara@gmail.com>
2021-02-09 20:02:44 +0100

    aff rb07 ex00

---
+++ rb07/ex01/your_namebook.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+def array_nombres(hash)
+  puts hash.value
+end
+
+personas = {
+  "jean" => "valjean",
+  "grace" => "hopper",
+  "xavier" => "neil",
+    "fifi" => "brindacier"
+}
+array = []
+
+personas.each do |key, value| 
+  array << key + ' ' + value
+end
+
+print array
